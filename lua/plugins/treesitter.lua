d0a99f29f0efa897cb932170c89d54c5afadab29
Calum <calum@mackenzie-chambers.co.uk>
2023-06-24 20:58:37 +0100

    feat: treesitter highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,46 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  version = false,
+  event = { "BufReadPost", "BufNewFile" },
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })
+  end,
+  cmd = { "TSUpdateSync" },
+  opts = {
+    highlight = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c_sharp",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+  },
+
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      local added = {}
+      opts.ensure_installed = vim.tbl_filter(function(lang)
+        if added[lang] then
+          return false
+        end
+        added[lang] = true
+        return true
+      end, opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
