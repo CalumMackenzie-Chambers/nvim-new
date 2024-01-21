f6233c387d56c277ebbe4f1dda304cf7acc1b353
Calum <calum@mackenzie-chambers.co.uk>
2024-01-21 17:25:41 +0000

    feat: transparent mode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,10 @@
 return {
   {
-    "rose-pine/neovim",
-    version = false,
-    name = "rose-pine",
-    opts = {
-      variant = "moon",
-      disable_background = false,
-      highlight_groups = {
-        NeoTreeDirectoryName = { fg = "rose" },
-        NeoTreeDirectoryIcon = { fg = "rose" },
-        NeoTreeTabInactive = { fg = "subtle", bg = "none" },
-        NeoTreeTabActive = { fg = "rose", bg = "surface" },
-        NeoTreeTabSeparatorInactive = { fg = "rose", bg = "none" },
-        NeoTreeTabSeparatorActive = { fg = "rose", bg = "surface" },
-        SignColumn = { bg = "none" },
-        GitSignsAdd = { fg = "pine", bg = "base" },
-        GitSignsChange = { fg = "rose", bg = "base" },
-        GitSignsDelete = { fg = "love", bg = "base" },
-      },
-    },
-    lazy = false,
-    -- init = function()
-    --   vim.cmd("colorscheme rose-pine")
-    -- end,
-  },
-  {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     opts = {
       contrast = "hard",
+      transparent_mode = true,
     },
     lazy = false,
     init = function()
