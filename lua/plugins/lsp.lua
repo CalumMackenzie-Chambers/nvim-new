1b93c1a835ebe05e288cb69ac4f3e60f97498aaa
Calum Mackenzie-Chambers <calum@pharmica.co.uk>
2024-01-22 11:05:56 +0000

    fix: diagnostic.open_float error message

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,7 +90,7 @@ return {
       keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
       keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
       keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-      keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()()<CR>", opts)
+      keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
       keymap(bufnr, "n", "<leader>vca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
       keymap(bufnr, "n", "<leader>vrn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
       keymap(bufnr, "n", "<leader>vft", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
