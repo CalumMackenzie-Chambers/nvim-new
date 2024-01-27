0a4b0a4e3fe22b8a7e134588218f0fc5d2c21616
Calum <calum@mackenzie-chambers.co.uk>
2024-01-27 22:14:04 +0000

    feat: turn off transparecny for linux

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     priority = 1000,
     opts = {
       contrast = "hard",
-      transparent_mode = true,
+      transparent_mode = false,
     },
     lazy = false,
     init = function()
