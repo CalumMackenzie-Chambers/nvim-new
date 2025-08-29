b22f4866eb5cc36188ccd2663daa4236c9d4cfbf
Calum Mackenzie-Chambers <calum@mackenzie-chambers.co.uk>
2025-08-29 15:05:17 +0100

    fix: remove enter key accepting completions

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -85,10 +85,7 @@ return {
       },
 
       keymap = {
-        preset = "enter",
-        ["<C-y>"] = { "select_and_accept" },
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        preset = "default",
       },
     }
   end,
